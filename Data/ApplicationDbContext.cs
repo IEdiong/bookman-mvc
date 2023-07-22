f82100643c794954721b9b13e46d8d207f37e5b3
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-07-22 17:22:46 +0100

    add seed data

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -6,11 +6,52 @@ namespace Bookman.Data;
 
 public class ApplicationDbContext : IdentityDbContext
 {
+    public DbSet<Book> Books { get; set; } = null!;
+
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
     }
 
-    public DbSet<Book> Books { get; set; } = null!;
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        List<Book> books = new()
+        {
+            new Book("Basics of C# Language")
+            {
+                Id = 1,
+                Author = "Vnicom Hub",
+                Description = "This book covers the basics of C#.",
+                Price = 5000,
+                Year = 2021
+            },
+            new Book("Fuji Music")
+            {
+                Id = 2,
+                Author = "Ade Ogunsanya",
+                Description = "Want to learn how to sing fuji jazz, then this book is for you.",
+                Price = 2000,
+                Year = 1998
+            },
+            new Book("Introduction to Backend development in C#")
+            {
+                Id = 3,
+                Author = "Ediong Joseph",
+                Description = "This book helps you master the basics of backend development with the C# language.",
+                Price = 10000,
+                Year = 2023
+            },
+            new Book("Introduction to Frontend development with Angular")
+            {
+                Id = 4,
+                Author = "Ediong Joseph",
+                Description = "This book helps you master the basics of frontend developement with Angular.",
+                Price = 10500,
+                Year = 2023
+            }
+        };
+        modelBuilder.Entity<Book>().HasData(books);
+        base.OnModelCreating(modelBuilder);
+    }
 }
 
---
+++ Migrations/20230722162230_AddSeedData.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookman.Migrations
+{
+    public partial class AddSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "Description", "Name", "Price", "Year" },
+                values: new object[,]
+                {
+                    { 1, "Vnicom Hub", "This book covers the basics of C#.", "Basics of C# Language", 5000m, 2021 },
+                    { 2, "Ade Ogunsanya", "Want to learn how to sing fuji jazz, then this book is for you.", "Fuji Music", 2000m, 1998 },
+                    { 3, "Ediong Joseph", "This book helps you master the basics of backend development with the C# language.", "Introduction to Backend development in C#", 10000m, 2023 },
+                    { 4, "Ediong Joseph", "This book helps you master the basics of frontend developement with Angular.", "Introduction to Frontend development with Angular", 10500m, 2023 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
