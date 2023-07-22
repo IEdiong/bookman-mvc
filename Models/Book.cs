985bc8a213039cf11cba260e710ee8c1d4565708
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-07-22 15:54:48 +0100

    add Book model

---
+++ Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bookman.Models
+{
+	public class Book
+	{
+		[Key]
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string? Author { get; set; }
+
+		public string? Description { get; set; }
+
+		public int Year { get; set; }
+
+		public decimal Price { get; set; }
+
+		public Book(string name)
+		{
+			Name = name;
+		}
+	}
+}
+
