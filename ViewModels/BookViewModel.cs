d5c0520b1db0ea7079ec6fe3f7b09dfad8ea266d
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-07-22 18:38:32 +0100

    add bookviewmodel

---
+++ ViewModels/BookViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Bookman.ViewModels
+{
+	public class BookViewModel
+	{
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Author { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public int Year { get; set; }
+
+        public double Price { get; set; }
+
+        
+	}
+}
+
