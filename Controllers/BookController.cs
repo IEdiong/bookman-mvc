6af354b8d40545b1f81ddd508240a5e21b4dcf7f
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-07-23 14:21:14 +0100

    add a view to create new book

---
+++ Controllers/BookController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bookman.Models;
+using Bookman.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookman.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BookController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /<controller>/
+        [HttpPost]
+        public IActionResult Create(BookViewModel model)
+        {
+            return View();
+        }
+    }
+}
+
